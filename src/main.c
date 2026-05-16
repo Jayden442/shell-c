8c374fac990b949cbabab22e875794e01178d1b3
Jayden <chengjayden442@gmail.com>
2026-05-16 13:02:53 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+static keep_looping = true;
+
+int check_if_exit(char *line) {
+  if (strncmp(line, "exit", 4) == 0) {
+    keep_looping = false;
+    return 1;
+  }
+  return 0;
+}
 
 int invalidInput(char *line) {
   line[strcspn(line, "\n")] = '\0';
@@ -16,6 +27,9 @@ int getUserInput() {
     invalidInput("");
   }
   else {
+    if (check_if_exit(line)) {
+      return 0;
+    }
     invalidInput(line);
   }
   return 1;
@@ -23,7 +37,8 @@ int getUserInput() {
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  while (1) {
+  
+  while (keep_looping) {
     printf("$ ");
     getUserInput();
   }
