e52074786139531885f79519326a1154c299fc28
Jayden <chengjayden442@gmail.com>
2026-05-20 19:55:46 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -162,7 +162,6 @@ int cd_cmd(char **args) {
     return 1;
   }
   int arg_len = strlen(args[1]);
-  printf("arg_len: %d", arg_len);
   char *homedir = getenv("HOME");
   int home_len = strlen(homedir);
   char cwd[arg_len + home_len];
