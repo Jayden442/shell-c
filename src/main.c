eab601e2a8db9140099bdf020d4012b5cc298b02
Jayden <chengjayden442@gmail.com>
2026-05-23 11:26:21 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -340,8 +340,7 @@ int parse_args(char **args) {
     int newStrExtra = 0;
     for (int i = 0; i < strlen(args[index]); i++) {
       if (i == high+1) {
-        start = high+1;
-        quote_pairs(&low, &high, &start, args[start]);
+        quote_pairs(&low, &high, &start, args[index]+start);
       }
       if (i == low || i == high) {
         newStrExtra--;
